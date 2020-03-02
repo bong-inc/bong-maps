a857d794e2e6f4efe3c68533babd8618fb83ded0
mfoman <frederikblunck@gmail.com>
2020-03-02 12:19:12 +0100

    Fix test-case to work with junit5

---
+++ src/test/java/bfst/FirstJunit5Test.java
@@ -0,0 +1,16 @@
+/**
+ * See link for examples.
+ * https://junit.org/junit5/docs/current/user-guide/
+ */
+package bfst;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class FirstJunit5Test {
+
+    @Test public void testAppHasAGreeting() {
+        assertEquals("hello", "hello");
+    }
+}
