bf04306c73f33d6443125907225a87944904c649
jglr <jglr@itu.dk>
2020-03-09 13:34:20 +0100

    Fix OSMReaderTest

--- src/test/java/bfst/OSMReaderTest.java
+++ src/test/java/bfst/OSMReaderTest.java
@@ -4,21 +4,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import bfst.controllers.FileTypeNotSupportedException;
 import bfst.controllers.MainController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
-public class OSMReaderTest {
+public class OSMReaderTest extends Application {
 
     @Test
     public void testOSMReaderThrowsException(){
         FileTypeNotSupportedException e = assertThrows(FileTypeNotSupportedException.class, () -> {
             File file = new File("build.gradle");
-            MainController.loadFile(file);
+            new MainController(new Stage()).loadFile(file);
         });
 
         String expected = ".gradle";
         String actual = e.getFileType();
         assertEquals(expected, actual);
     }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
 }
