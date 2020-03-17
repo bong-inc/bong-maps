7d91ee559e9a60afe10617c6eab602ac3b76d55d
emja <emja@itu.dk>
2020-03-17 18:20:57 +0100

    Fixes to Coastlines and Relations
    Add land if no coastline exsists

--- src/main/java/bfst/OSMReader/OSMReader.java
+++ src/main/java/bfst/OSMReader/OSMReader.java
@@ -88,19 +88,28 @@ public class OSMReader {
                                 type = Type.UNKNOWN;
                                 break;
                             case "relation":
+                                relationHolder.collectRelation();
                                 if(!drawableByType.containsKey(type)) drawableByType.put(type, new ArrayList<>());
-                                drawableByType.get(type).add(new PolyLinePath(relationHolder, type));
+                                if(relationHolder.getWays() != null) drawableByType.get(type).add(new PolyLinePath(relationHolder, type));
                                 type = Type.UNKNOWN;
                                 break;
                             case "osm":
                                 ArrayList<Drawable> coastlines = new ArrayList<>();
                                 for(Map.Entry<Node,Way> entry : tempCoastlines.entrySet()){
                                     if(entry.getValue().first() == entry.getValue().last()){
-                                        coastlines.add(new LinePath(entry.getValue(),Type.COASTLINE));
+                                        coastlines.add(new LinePath(entry.getValue(), Type.COASTLINE));
                                     } else {
                                         fixCoastline(entry.getValue());
                                         coastlines.add(new LinePath(entry.getValue(), Type.COASTLINE));
                                     }
+                                }
+                                if(coastlines.size() == 0){
+                                    Way land = new Way();
+                                    land.addNode(new Node(0, bound.getMinLon(), bound.getMinLat()));
+                                    land.addNode(new Node(0, bound.getMinLon(), bound.getMaxLat()));
+                                    land.addNode(new Node(0, bound.getMaxLon(), bound.getMaxLat()));
+                                    land.addNode(new Node(0, bound.getMaxLon(), bound.getMinLat()));
+                                    coastlines.add(new LinePath(land, Type.COASTLINE));
                                 }
                                 drawableByType.put(Type.COASTLINE,coastlines);
                                 break;
@@ -206,8 +215,19 @@ public class OSMReader {
                 break;
             case "way":
                 long memberRef = Long.parseLong(reader.getAttributeValue(null, "ref"));
-                if (tempWays.get(memberRef) != null)
-                    relationHolder.addWay(tempWays.get(memberRef));
+                if (tempWays.get(memberRef) != null) {
+                    switch (reader.getAttributeValue(null, "role")) {
+                        case "outer":
+                            relationHolder.addToOuter(tempWays.get(memberRef));
+                            break;
+                        case "inner":
+                            relationHolder.addToInner(tempWays.get(memberRef));
+                            break;
+                        default:
+                            relationHolder.addWay(tempWays.get(memberRef));
+                            break;
+                    }
+                }
                 break;
             case "relation":
                 relationHolder.addRefId(Long.parseLong(reader.getAttributeValue(null, "ref")));
--- src/main/java/bfst/OSMReader/Relation.java
+++ src/main/java/bfst/OSMReader/Relation.java
@@ -9,12 +9,16 @@ public class Relation implements LongSupplier {
     private ArrayList<Way> ways;
     private ArrayList<Long> ids;
     private ArrayList<Relation> relations;
+    private ArrayList<Way> outer;
+    private ArrayList<Way> inner;
 
     public Relation(){
         nodes = new ArrayList<>();
         ways = new ArrayList<>();
         relations = new ArrayList<>();
         ids = new ArrayList<>();
+        inner = new ArrayList<>();
+        outer = new ArrayList<>();
     }
 
     public Relation(long id){
@@ -23,6 +27,16 @@ public class Relation implements LongSupplier {
         ways = new ArrayList<>();
         relations = new ArrayList<>();
         ids = new ArrayList<>();
+        inner = new ArrayList<>();
+        outer = new ArrayList<>();
+    }
+
+    public void addToOuter(Way way) {
+        outer.add(way);
+    }
+
+    public void addToInner(Way way){
+        inner.add(way);
     }
 
     public void addNode(Node node){ nodes.add(node); }
@@ -31,10 +45,50 @@ public class Relation implements LongSupplier {
     public void addRelation(Relation relation){ relations.add(relation); }
 
     public ArrayList<Way> getWays(){ return ways; }
-    public ArrayList<Long> getIds(){return ids;}
+    public ArrayList<Long> getIds(){ return ids; }
 
     public void nullifyIds(){
         ids = null;
+    }
+
+    public void collectRelation(){
+        int counter = 0;
+        int amtOuter =  outer.size();
+        ArrayList<Way> tempOuters = new ArrayList<>();
+        ArrayList<Way> resOuters = new ArrayList<>();
+        while(counter < amtOuter){
+            Way resOuter = null;
+            for(int i = counter; i < amtOuter; i++){
+                try{
+                    resOuter = Way.merge(resOuter, outer.get(i));
+                    counter++;
+                } catch (IllegalArgumentException e) {
+                    continue;
+                }
+            }
+            if(resOuter != null) {
+                if(resOuter.last() == resOuter.first()){
+                    resOuters.add(resOuter);
+                }
+                else{
+                    tempOuters.add(resOuter);
+                }
+            }
+        }
+
+        for(int i = 0; i < tempOuters.size(); i++){
+            Way resOuter = tempOuters.get(i);
+            for(int o = i; o < tempOuters.size(); o++){
+                try{
+                    resOuter = Way.merge(resOuter, tempOuters.get(o));
+                } catch (IllegalArgumentException e) {
+                    continue;
+                }
+            }
+            resOuters.add(resOuter);
+        }
+        ways.addAll(resOuters);
+        ways.addAll(inner);
     }
 
     @Override
