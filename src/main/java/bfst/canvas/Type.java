58f851d455e57a1a96c7063fe2987d6895b2a044
Jonas Røssum <jonas.g.roessum@gmail.com>
2020-03-22 14:46:38 +0100

    Update mmx min thresholdsSince we switched to Mercator, we need to updatethese thresholds to their corresponding value in Mercator projection

--- src/main/java/bfst/canvas/Type.java
+++ src/main/java/bfst/canvas/Type.java
@@ -7,18 +7,19 @@ import java.io.Serializable;
 
 public enum Type implements Serializable {
 
-    UNKNOWN("", new String[]{""},Color.BLACK,1,false, Color.BLACK, 20000),
-    COASTLINE("natural",new String[]{"coastline"},Color.valueOf("#f0f0f0"),0,true, Color.valueOf("#f0f0f0"), 1),
-    RESIDENTIAL("landuse",new String[]{"residential","industrial"},Color.valueOf("#e8e8e8"),0,true, Color.valueOf("#b3b3b3"), 1000),
-    FARMFIELD("landuse",new String[]{"farmland"},Color.valueOf("#f7f6eb"),0,true, Color.valueOf("#f2ff00"), 3000),
-    BEACH("natural",new String[]{"beach"},Color.BEIGE,0,true, Color.valueOf("#f8ff70"), 4000),
-    FOREST("landuse",new String[]{"forest","meadow","grass"},Color.valueOf("#c8f2bb"),0,true, Color.valueOf("#04ff00"), 3000),
-    NATURALS("natural",new String[]{"scrub","grassland","heath"},Color.valueOf("#c8f2bb"),0,true, Color.valueOf("#04ff00"), 6000),
-    LEISURE("leisure",new String[]{"park"},Color.valueOf("#c8f2bb"),0,true, Color.valueOf("#04ff00"), 6000),
-    WATER("natural",new String[]{"water"},Color.valueOf("#ade1ff"),0,true, Color.AQUA, 2000),
-    WATERWAY("waterway",new String[]{""},Color.valueOf("#ade1ff"),1,false, Color.AQUA, 10000),
-    RAILWAY("railway",new String[]{"rail","light_rail","subway"}, Color.DARKGREY,1,false, Color.valueOf("#da21ff"), 500),
-    BUILDING("building",new String[]{""},Color.valueOf("#dbdbdb"),0,true, Color.valueOf("#ff7c3b"), 20000)/*,
+    UNKNOWN("", new String[]{""},Color.BLACK,1,false, Color.BLACK, 0.1000246664f),
+    COASTLINE("natural",new String[]{"coastline"},Color.valueOf("#f0f0f0"),0,true, Color.valueOf("#f0f0f0"), 0.000005001233322f),
+    RESIDENTIAL("landuse",new String[]{"residential","industrial"},Color.valueOf("#e8e8e8"),0,true, Color.valueOf("#b3b3b3"), 0.005001233322f),
+    FARMFIELD("landuse",new String[]{"farmland"},Color.valueOf("#f7f6eb"),0,true, Color.valueOf("#f2ff00"), 0.01500369996f),
+    BEACH("natural",new String[]{"beach"},Color.BEIGE,0,true, Color.valueOf("#f8ff70"), 0.02000493329f),
+    FOREST("landuse",new String[]{"forest","meadow","grass"},Color.valueOf("#c8f2bb"),0,true, Color.valueOf("#04ff00"), 0.01500369996f),
+    NATURALS("natural",new String[]{"scrub","grassland","heath"},Color.valueOf("#c8f2bb"),0,true, Color.valueOf("#04ff00"), 0.03000739993f),
+    LEISURE("leisure",new String[]{"park"},Color.valueOf("#c8f2bb"),0,true, Color.valueOf("#04ff00"), 0.03000739993f),
+    WATER("natural",new String[]{"water"},Color.valueOf("#ade1ff"),0,true, Color.AQUA, 0.01000246664f),
+    WATERWAY("waterway",new String[]{""},Color.valueOf("#ade1ff"),1,false, Color.AQUA, 0.05001233322f),
+    RAILWAY("railway",new String[]{"rail","light_rail","subway"}, Color.DARKGREY,1,false, Color.valueOf("#da21ff"), 0.002500616661f),
+    BUILDING("building",new String[]{""},Color.valueOf("#dbdbdb"),0,true, Color.valueOf("#ff7c3b"), 0.1000246664f)
+    /*
     PRIMARY_ROAD("highway",new String[]{"primary","motorway"},Color.YELLOW,1.5,false, Color.YELLOW, 1),
     SECONDARY_ROAD("highway",new String[]{"secondary"},Color.WHITE,1,false, Color.valueOf("#6e7fff"), 1000),
     TERTIARY_ROAD("highway",new String[]{"tertiary"},Color.WHITE,1,false, Color.valueOf("#6e7fff"), 10000),
@@ -40,9 +41,9 @@ public enum Type implements Serializable {
     private final boolean shouldHaveFill;
     private final boolean shouldHaveStroke;
     private final Color alternateColor;
-    private final int minMxx;
+    private final float minMxx;
 
-    Type(String key, String[] value, Color color, double width, boolean shouldHaveFill, Color alternateColor, int minMxx) {
+    Type(String key, String[] value, Color color, double width, boolean shouldHaveFill, Color alternateColor, float minMxx) {
         this.key = key;
         this.value = value;
         this.color = color;
@@ -78,7 +79,7 @@ public enum Type implements Serializable {
         return shouldHaveStroke;
     }
 
-    public int getMinMxx() {
+    public float getMinMxx() {
         return minMxx;
     }
 }
