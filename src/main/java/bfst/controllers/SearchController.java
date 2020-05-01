4f70702f43cdad2acd8e69fedade4e56caf0dd18
KristofferHojelse <34659757+KristofferHojelse@users.noreply.github.com>
2020-05-01 18:10:18 +0200

    Update SearchController

--- src/main/java/bfst/controllers/SearchController.java
+++ src/main/java/bfst/controllers/SearchController.java
@@ -24,27 +24,39 @@ public class SearchController {
     this.tempQuery = newTempQuery;
   }
 
-  public ArrayList<Address> getBestMatches(String query, List<Address> addresses) {
+  public ArrayList<Address> getBestMatches(String query, List<Address> addresses, int numberOfMatches) {
     query = query.toLowerCase();
     Address inputAdress = null;
     ArrayList<Address> tempBest = new ArrayList<>();
-    
+
     try {
       inputAdress = Address.parse(query);
       int index = Collections.binarySearch(addresses, inputAdress);
       tempBest = new ArrayList<>();
-      for (int i = 0; i < 5; i++) {
+      for (int i = 0; i < numberOfMatches; i++) {
         if (index < 0) {
           tempBest.add(addresses.get(-index - 1 + i));
         } else {
           tempBest.add(addresses.get(index + i));
         }
       }
-      
+
     } catch (InvalidAddressException e) {
       System.out.println("invalid address");
     }
 
     return tempBest;
   }
+
+  // public int[] matches(String query, String address) {
+  //   String regex = ".*?(?<match>" + query.toLowerCase() + ").*";
+  //   Pattern pattern = Pattern.compile(regex);
+  //   Matcher m = pattern.matcher(address.toLowerCase());
+  //   if (m.find() && m.group("match") != null && query.length() > 0) {
+  //     return new int[] { m.start("match"), m.end("match") };
+  //   } else {
+  //     return null;
+  //   }
+  // }
+
 }
