3b440cc3f1e5e98ab8d55ca6ee49118c82405cd4
KristofferHojelse <34659757+KristofferHojelse@users.noreply.github.com>
2020-05-02 04:30:06 +0200

    Remove model from searchcontroller

--- src/main/java/bfst/controllers/SearchController.java
+++ src/main/java/bfst/controllers/SearchController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import bfst.OSMReader.Model;
 import bfst.addressparser.Address;
 import bfst.addressparser.InvalidAddressException;
 
